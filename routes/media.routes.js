2438a5520d2b3087f7235990b400468ec3685beb
“nameisjar” <nthia4@gmail.com>
2023-11-05 14:04:34 +0700

    test

--- routes/media.routes.js
+++ routes/media.routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const { image } = require('../libs/multer');
 const { createImage, getAllImage, getDetailImage, deleteImage, updateImage } = require('../controllers/media.controllers');
 
-
 router.post('/image', image.single('image'), createImage);
 router.get('/image', getAllImage);
 router.get('/image/:id', getDetailImage);
