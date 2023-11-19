4e8771eda052f06cc76bb59b3274a59bf559fdd7
“nameisjar” <nthia4@gmail.com>
2023-11-19 10:22:02 +0700

    railway app

--- prisma/migrations/20231105060300_first_migration/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Image" (
-    "id" SERIAL NOT NULL,
-    "judul" TEXT NOT NULL,
-    "deskripsi" TEXT NOT NULL,
-    "url_gambar" TEXT NOT NULL,
-
-    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
-);
